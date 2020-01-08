beaa277f8c0cdda9165a1b03feb6f544bb9b99a2
francis.wu <peteeelol@gmail.com>
2020-01-08 14:05:13 +0800

    Make it better

---
+++ main.py
@@ -0,0 +1,6 @@
+from data_structure_sample.data_structures.heap import Heap
+
+
+if __name__ == "__main__":
+    a = Heap()
+    pass
--- tests/test_heap.py
+++ tests/test_heap.py
@@ -1,38 +1,48 @@
 import unittest
-from data_structure_sample.data_structures.heap import Heap, HeapUtils
-import heapq
+#from data_structure_sample.data_structures.heap import Heap, HeapUtils
+#from data_structure_sample.data_structures import myheapq
+#import heapq
 
 
-class HeapTest(unittest.TestCase):
+# class HeapTest(unittest.TestCase):
 
-    def test_1(self):
-        Heap_real = []
-        Heap_test = Heap()
-        for i in [Heap_real, Heap_test]:
-            for j in range(10):
-                i.append(j*j)
-        for i in [Heap_real, Heap_test]:
-            for j in range(10):
-                i.append(j*j)
-        for i in [Heap_real, Heap_test]:
-            for j in range(10):
-                i.append(j*j)
-        hu = HeapUtils()
-        heapq.heapify(Heap_real)
-        self.assertTrue(hu.is_minheap(Heap_real))
-        self.assertTrue(hu.is_minheap(Heap_test))
+#     def test_1(self):
+#         Heap_real = [1, 2, 3]
+#         self.assertTrue(not myheapq.ismaxheap(Heap_real))
+#         self.assertTrue(myheapq.isminheap(Heap_real))
 
-    def test_2(self):
-        Heap_real = []
-        Heap_test = Heap()
-        n = 40
-        for j in range(n):
-            Heap_real.append(j)
-            Heap_test.append(j)
-        for j in range(n):
-            Heap_real.append(j)
-            Heap_test.append(j)
-        hu = HeapUtils()
-        heapq.heapify(Heap_real)
-        self.assertTrue(hu.is_minheap(Heap_real))
-        self.assertTrue(hu.is_minheap(Heap_test))
+#     def test_2(self):
+#         Heap_real = [3, 2, 1]
+#         self.assertTrue(myheapq.ismaxheap(Heap_real))
+#         self.assertTrue(not myheapq.isminheap(Heap_real))
+
+#     def test_3(self):
+#         Heap_test = [2, 3, 1]
+#         self.assertTrue(not myheapq.ismaxheap(Heap_test))
+#         self.assertTrue(not myheapq.isminheap(Heap_test))
+
+#     def test_4(self):
+#         Heap_real = [i for i in range(200)]
+#         self.assertTrue(not myheapq.ismaxheap(Heap_real))
+#         self.assertTrue(myheapq.isminheap(Heap_real))
+
+#     def test_5(self):
+#         Heap_real = [i for i in range(200, 0, -1)]
+#         self.assertTrue(myheapq.ismaxheap(Heap_real))
+#         self.assertTrue(not myheapq.isminheap(Heap_real))
+
+#     def test_6(self):
+#         Heap_test = [i for i in range(100, 200)] + \
+#             [i for i in range(100, 0, -1)]
+#         self.assertTrue(not myheapq.ismaxheap(Heap_test))
+#         self.assertTrue(not myheapq.isminheap(Heap_test))
+
+#     def test_7(self):
+#         Heap_test = [i for i in range(10, 20)] + \
+#             [i for i in range(10, 0, -1)]
+#         self.assertTrue(not myheapq.ismaxheap(Heap_test))
+#         self.assertTrue(not myheapq.isminheap(Heap_test))
+#         myheapq.min_heapify(Heap_test)
+#         print(Heap_test)
+#         self.assertTrue(not myheapq.ismaxheap(Heap_test))
+#         self.assertTrue(myheapq.isminheap(Heap_test))
